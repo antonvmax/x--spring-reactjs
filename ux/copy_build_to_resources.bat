9979b5744c8c40557214396df97b96b62c7b99f8
antonvmax <antonvmax@ya.ru>
2018-06-08 15:50:27 +0500

    add `copy_build_to_resources` to npm's `build` stage
    It will copy builded files into `resources` folder.
    So React app will be packed into Spring app.

---
+++ ux/copy_build_to_resources.bat
@@ -0,0 +1,4 @@
+﻿del /Q ..\src\main\resources\static\static\js\*
+del /Q ..\src\main\resources\static\static\css\*
+del /Q ..\src\main\resources\static\static\media\*
+xcopy /e /y build\* ..\src\main\resources\static\
