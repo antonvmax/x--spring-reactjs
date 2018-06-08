a65f1bf7790976800c06351dd3193199784befd0
antonvmax <antonvmax@ya.ru>
2018-06-08 10:28:43 +0500

    XSpringReactjs :: Spring Boot, Maven
    + Web

---
+++ src/main/java/pro/antonvmax/xspringreactjs/XSpringReactjsApplication.java
@@ -0,0 +1,12 @@
+package pro.antonvmax.xspringreactjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XSpringReactjsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XSpringReactjsApplication.class, args);
+	}
+}
---
+++ src/test/java/pro/antonvmax/xspringreactjs/XSpringReactjsApplicationTests.java
@@ -0,0 +1,16 @@
+package pro.antonvmax.xspringreactjs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XSpringReactjsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
