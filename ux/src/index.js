71955127116bdc7c2eddd968cda0566d5f4fc848
antonvmax <antonvmax@ya.ru>
2018-06-13 15:33:47 +0500

    use `babel-polyfill` __ fix old IE error "SCRIPT5009: "Map" not defined"

--- ux/src/index.js
+++ ux/src/index.js
@@ -1,3 +1,7 @@
+// Warning: React depends on requestAnimationFrame.
+// Make sure that you load a polyfill in older browsers. http://fb.me/react-polyfills
+import 'babel-polyfill';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
