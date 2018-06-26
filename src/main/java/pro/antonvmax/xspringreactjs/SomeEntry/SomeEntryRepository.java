72db3d2d6c4aa36f95ecd0dbda07cff89cc88d26
antonvmax <antonvmax@ya.ru>
2018-06-26 15:41:51 +0500

    SomeEntry⁞Repository __ [add] repository for SomeEntry

---
+++ src/main/java/pro/antonvmax/xspringreactjs/SomeEntry/SomeEntryRepository.java
@@ -0,0 +1,6 @@
+package pro.antonvmax.xspringreactjs.SomeEntry;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SomeEntryRepository extends JpaRepository<SomeEntry, String> {
+}
