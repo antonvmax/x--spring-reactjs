49eb5bdc944d8b2373948bd6be0d7e4e70f45c60
antonvmax <antonvmax@ya.ru>
2018-07-12 16:40:10 +0500

    [spec] App⁞test __ actualize (repair) test to use redux
    almost copy-paste changes from index.js

--- ux/src/App.test.js
+++ ux/src/App.test.js
@@ -2,8 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/R_index';
+import thunk from 'redux-thunk';
+
+const middleware = [ thunk ];
+
+const store = createStore(
+    rootReducer,
+    applyMiddleware(...middleware)
+);
+
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+    const div = document.createElement('div');
+    ReactDOM.render(
+        <Provider store={store}>
+            <App/>
+        </Provider>,
+        div
+    );
+    ReactDOM.unmountComponentAtNode(div);
 });
